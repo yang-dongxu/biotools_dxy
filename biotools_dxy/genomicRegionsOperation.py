b349feab7140ac60fd4414a8c27a48aa86548abb
yang-dongxu <yang_dongxu@qq.com>
2023-05-31 23:02:50 +0800

    modified:   biotools_dxy/genomicRegionsOperation.py

--- biotools_dxy/genomicRegionsOperation.py
+++ biotools_dxy/genomicRegionsOperation.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
 import bioframe as bf 
+from bioframe import *
 import logging
+from functools import partial, wraps
+import inspect
 
 def __get_fix_position(
     df: pd.DataFrame,
@@ -101,3 +104,93 @@ def resize(
 
     return df
 
+
+class BioFrameMethods():
+    methods_for_overwrite = [
+        "is_bedframe",
+        "assign_view","closest","cluster","complement","count_overlaps","coverage"
+        "expand","merge","overlap","select","select_indices","select_labels","select_mask",
+        "setdiff","sort_bedframe","subtract","trim"
+        ]
+    params_check = ["chrom_col", "start_col", "end_col", "strand_col"]
+
+
+    def __init__(self,parent) -> None:
+        self.parent = parent
+        self.info = dict(
+            chrom_col = self.parent._chrom_col, 
+            start_col = self.parent._start_col, 
+            end_col = self.parent._end_col , 
+            strand_col = self.parent._strand_col
+            )
+        self.cols = (self.parent._chrom_col, self.parent._start_col, self.parent._end_col)
+
+        methods_list = [method_name for method_name in dir(bf) if callable(getattr(bf, method_name))]
+        for method_name in methods_list:
+            if method_name in self.methods_for_overwrite:
+                setattr(self, method_name, self.wrap_params(getattr(bf, method_name)))
+            else:
+                setattr(self, method_name, getattr(bf, method_name))
+
+
+    def wrap_cols(self,option="cols",**kwargs):
+        params_dict = {option:self.cols}
+        params_dict.update(kwargs)
+        return params_dict
+
+    def wrap_infos(self,**kwargs):
+        params_dict = self.info.copy()
+        params_dict.update(kwargs)
+        return params_dict
+    
+
+    def wrap_params(self,func, *args, **kwargs):
+        sig = inspect.signature(func)
+        params = sig.parameters
+        if len( set(self.params_check) & set(params.keys()) ) != 0:
+            new_func =  partial(func, self.parent, *args, **self.wrap_infos(**kwargs))
+        else:
+            if "cols1" in params.keys():
+                new_func =  partial(func, self.parent, *args, **self.wrap_cols(option="cols1", **kwargs))
+            elif "cols" in params.keys():
+                new_func =  partial(func, self.parent, *args, **self.wrap_cols(option="cols",**kwargs))
+        new_func.__doc__ = func.__doc__
+        return new_func
+            
+         
+    def resize(self, size, fix = "center", *args, **kwargs):
+        return self.wrap_params(resize)(size, fix, *args, **kwargs)
+
+    
+
+
+class BioRegions(pd.DataFrame):
+    params_kept = ["_chrom_col", "_start_col", "_end_col", "_strand_col"]
+    params_kept_default = ["chrom", "start", "end", "strand"]
+
+    def __init__(self,*args,**kwargs) -> None:
+        raw_params = kwargs.copy()
+
+        for param in BioRegions.params_kept:
+            if param in kwargs.keys():
+                del kwargs[param]
+        super().__init__(*args,**kwargs)
+
+        for param, pdault in zip(BioRegions.params_kept, BioRegions.params_kept_default):
+            self.__setattr__(param, pdault)
+            if param in raw_params.keys():
+                setattr(self, param, kwargs[param])
+
+
+        for param in BioRegions.params_kept:
+            assert getattr(self, param) in self.columns, f"{param=} not in columns! The column name should be set by {param} = 'column_name'"
+
+        self.bf = BioFrameMethods(self)
+
+@wraps
+def wrap_bf(func):
+    def wrapper(*args, **kwargs):
+        return BioRegions(func(*args, **kwargs))
+    return wrapper
+
+read_table = wrap_bf(bf.read_table)
